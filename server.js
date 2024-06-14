8dace833f5d3eb7eea3cdb64a6afecd9cc2633e5
Patrick Peterson <ppeterson609@gmail.com>
2024-06-14 08:33:37 -0400

    updated routes

--- server.js
+++ server.js
@@ -16,4 +16,47 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
-  
+  //API Routes
+  app.get("/notes", (req,res) => {
+    fs.readFile("./db/db.json", "utf8", (err,data) => {
+        if (err) throw err; 
+        res.json(JSON.parse(data));
+  });
+  });
+
+  app.post("/api/notes", (req,res) => {
+    const newNote = { id: uuidv4(), ...req.body };
+    fs.readFile("./db/db.json", "utf8", (err,data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        notes.push(newNote); 
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        if (err) throw err;
+        res.json(newNote);
+        });
+    });
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    const noteId = req.params.id;
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Error reading data" });
+      }
+      const notes = JSON.parse(data);
+      const filteredNotes = notes.filter((note) => note.id !== noteId);
+      fs.writeFile("./db/db.json", JSON.stringify(filteredNotes), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ message: "Error writing data" });
+        }
+        res.json({ message: "Note deleted successfully" });
+      });
+    });
+  });
+
+  //start the server
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
